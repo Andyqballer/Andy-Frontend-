cfadac0e6b1bf10e1f2deb97ac727276ae8a2b78
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-04-27 11:54:12 +0000

    frontend updated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,29 @@
 'use client';
 import Image from "next/image";
 import { tableData } from "./mockdata";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+
+  const tableActions = (e: React.MouseEvent<HTMLDivElement | HTMLTableRowElement>, type: string, userId: number) => {
+    e.stopPropagation();
+
+    switch(type){
+        case 'view table':
+            router.push(`/dashboard/${userId}`)
+            return;
+        case 'delete':
+            alert('Deleting user')
+            return;
+        case 'update':
+            alert('updating user')
+            return;
+        default:
+            return;
+    }
+}
     return (
       <div className="w-full h-full flex-1 px-[3rem] py-[1.3rem] bg-white">
         <h2 className="text-[1.3rem] pb-[1rem] font-medium">Role administration</h2>
@@ -19,40 +40,42 @@ export default function Page() {
 
             <div className="relative py-[2rem] overflow-x-auto">
                 <table className="w-full text-left">
-                  <thead>
-                    <tr >
-                      <th className="p-2 font-medium text-[1.1rem]">Username</th>
-                      <th className="p-2 font-medium text-[1.1rem]">Email</th>
-                      <th className="p-2 font-medium text-[1.1rem]">Login</th>
-                      <th className="p-2 font-medium text-[1.1rem]">Role</th>
-                      <th className="p-2 font-medium text-[1.1rem]">Actions</th>
+                  <thead >
+                    <tr className="border-b border-gray-300">
+                      <th className="px-2 pt-2 pb-[0.8rem] font-medium text-[1.1rem]">Username</th>
+                      <th className="px-2 pt-2 pb-[0.8rem] font-medium text-[1.1rem]">Email</th>
+                      <th className="px-2 pt-2 pb-[0.8rem] font-medium text-[1.1rem]">Login</th>
+                      <th className="px-2 pt-2 pb-[0.8rem] font-medium text-[1.1rem]">Role</th>
+                      <th className="px-2 pt-2 pb-[0.8rem] font-medium text-[1.1rem]">Actions</th>
                     </tr>
                   </thead>
                   <tbody>
                     {
                       tableData.map((data, index) => (
-                        <tr key={index} className="hover:bg-indigo-300">
-                          <td className="p-2 flex justify-start items-center gap-[1rem]">
-                            <div>
-                              <input type="checkbox" />
-                            </div>
-                              <Image src="/img/profile.jpeg" alt="Profile" width={50} height={50} className="rounded-full"/>
-                            <div className="font-medium ">{data.name}</div>
-                          </td>
-                          <td className="p-2 ">{data.email}</td>
-                          <td className="p-2 ">{data.login}</td>
-                          <td className="p-2 ">{data.role}</td>
-                          <td className="p-2 flex justify-start items-center gap-[1rem]">
-                            <div>
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                              </svg>
-                            </div>
-                            <div className="text-[0.9rem]">
-                              View user
-                            </div>
-                          </td>
-                        </tr>
+                        // <Link className="w-full" href={`/dashboard/${data.id}`}>
+                          <tr onClick={(e) => tableActions(e, 'view table', data.id)} key={index} className="w-full hover:bg-indigo-300 cursor-pointer">
+                            <td className="p-2 flex justify-start items-center gap-[1rem]">
+                              <div>
+                                <input type="checkbox" />
+                              </div>
+                                <Image src="/img/profile.jpeg" alt="Profile" width={50} height={50} className="rounded-full"/>
+                              <div className="font-medium ">{data.name}</div>
+                            </td>
+                            <td className="px-2 pb-[1.4rem] pt-[1.3rem] ">{data.email}</td>
+                            <td className="px-2 pb-[1.4rem] pt-[1.3rem] ">{data.login}</td>
+                            <td className="px-2 pb-[1.4rem] pt-[1.3rem] ">{data.role}</td>
+                            <td className="px-2 pb-[1.4rem] pt-[1.3rem] flex justify-start items-center gap-[1rem]">
+                              <div onClick={e=> tableActions(e, 'delete', data.id)} className="text-red-600 font-medium">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+                                  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                </svg>
+                              </div>
+                              <div onClick={e=> tableActions(e, 'update', data.id)} className="p-2 bg-gray-300 text-[0.8rem] font-medium cursor-pointer rounded-md">
+                                Update user
+                              </div>
+                            </td>
+                          </tr>
+                        // </Link>
                       ))
                     }
                   </tbody>
--- middleware.js
+++ middleware.js
@@ -6,16 +6,16 @@ export function middleware(request) {
   const { pathname } = request.nextUrl;
 
   // 如果访问dashboard且未登录，则重定向到登录页面
-  if (pathname.startsWith('/dashboard')) {
-    const token = request.cookies.get('token');
-    console.log(token);
-    if (!token) {
-      // Assuming you want to redirect to the home page
-      const url = nextUrl.clone();
-      url.pathname = '/';
-      return NextResponse.redirect(url);
-    }
-  }
+  // if (pathname.startsWith('/dashboard')) {
+  //   const token = request.cookies.get('token');
+  //   console.log(token);
+  //   if (!token) {
+  //     // Assuming you want to redirect to the home page
+  //     const url = nextUrl.clone();
+  //     url.pathname = '/';
+  //     return NextResponse.redirect(url);
+  //   }
+  // }
 
   return NextResponse.next();
 }
