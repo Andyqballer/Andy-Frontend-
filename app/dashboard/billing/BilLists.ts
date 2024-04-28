0efdd0f0f1f711a5a20b6742a1ce740f0a1e2954
Andyqballer <Andrew.quansah@uenr.edu.gh>
2024-04-28 09:08:31 +0000

    Feat: billing

---
+++ app/dashboard/billing/BilLists.ts
@@ -0,0 +1,23 @@
+export const billList = [
+    {
+        id: 1, 
+        name: 'Agyemang Florence',
+        task: 'Joshua Tech',
+        hours: 7,
+        amount: 64
+    },
+    {
+        id: 1, 
+        name: 'Agyemang Florence',
+        task: 'Joshua Tech',
+        hours: 7,
+        amount: 64
+    },
+    {
+        id: 1, 
+        name: 'Agyemang Florence',
+        task: 'Joshua Tech',
+        hours: 7,
+        amount: 64
+    },
+]
